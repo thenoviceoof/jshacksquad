aeaf860dbd8166e3ae91044e34992fc59af3faa3
Nathan Hwang <thenoviceoof@gmail.com>
2011-04-01 17:11:23 -0400

    added files thus far

---
+++ hs-grid.js
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------
+// hs-grid.js
+// --------------------------------------------------------------------
+
+function slide() {
+}
+
+$(document).ready(function() {
+	$("nav#left").onclick(function(e){
+		alert("hello world!");
+	    });
+	$("nav#right").onclick(function(e){
+		alert("hello world!");
+	    });
+    });
